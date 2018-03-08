523b460fdd0ffe268adf1424b782a2f2093d6a4a
algo <python@blogdaily.net>
2018-03-08 15:21:46 +0800

    version 0.2

--- erlang-config.el
+++
@@ -1,7 +0,0 @@
-;; erlang mode
-
-(defun enable-erlang-mode ()
-    (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.9.1/emacs" load-path))
-    (setq erlang-root-dir "/usr/local/lib/erlang/")
-    (setq exec-path (cons "/usr/local/lib/erlang/bin/" exec-path))
-    (require 'erlang-start))
