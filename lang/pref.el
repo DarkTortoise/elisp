ba91ab024534468b38f905f46afee85c160625f3
zhaoxiang <python@blogdaily.net>
2018-03-14 16:42:01 +0800

    version 0.3

--- lang/pref.el
+++ lang/pref.el
@@ -5,6 +5,12 @@
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(defvar packages '(use-package))
+
+(dolist (p packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
 ;; enable ibuffer
 (defalias 'list-buffers 'ibuffer)
 
