93e2765423dbc3913fa7f11c827bd91d25d56d52
zhao xiang <python@blogdaily.net>
2018-03-21 14:39:42 +0800

    version 0.3

--- lang/python.el
+++ lang/python.el
@@ -14,11 +14,7 @@
     :config
     (add-hook 'python-mode-hook 'anaconda-mode)
     (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-    (use-package company-anaconda
-      :config
-      (eval-after-load "company"
-	'(add-to-list 'company-backends 'company-anaconda))
-      )
+    
     )
   )
 
