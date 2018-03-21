45eb8892ab2958ec659c58190abfb0e9b3053c4d
zhao xiang <python@blogdaily.net>
2018-03-21 14:34:13 +0800

    version 0.3

--- lang/python.el
+++ lang/python.el
@@ -1,18 +1,25 @@
 ;;; python mode
 
+";;; Code: enable python mode."
 (defun enable-python-mode ()
-  ";;; Code: python mode."
-  (defvar packages '(elpy))
+  ";;; Code: define packages needed."
+  (defvar packages '(anaconda-mode
+		     company-anaconda))
 
   (dolist (p packages)
     (unless (package-installed-p p)
       (package-install p)))
-
-  (use-package elpy
-    :init
-    (progn
-      (elpy-enable)
-      (setq elpy-rpc-python-command "python3.6")
-      (setq python-shell-interpreter "python3.6"))
+  
+  (use-package anaconda-mode
+    :config
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    (use-package company-anaconda
+      :config
+      (eval-after-load "company"
+	'(add-to-list 'company-backends 'company-anaconda))
+      )
     )
   )
+
+;;; python.el ends here
