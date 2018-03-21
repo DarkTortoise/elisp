2e4a515e094cf5a615fb4823199b20c5f83e69a0
zhao xiang <python@blogdaily.net>
2018-03-21 15:25:52 +0800

    python use anaconda mode

--- lang/python.el
+++ lang/python.el
@@ -1,21 +1,33 @@
 ;;; python mode
 
-";;; Code: enable python mode."
-(defun enable-python-mode ()
-  ";;; Code: define packages needed."
+
+(defun check-packages ()
   (defvar packages '(anaconda-mode
 		     company-anaconda))
 
   (dolist (p packages)
     (unless (package-installed-p p)
       (package-install p)))
-  
+  )
+
+(defun anaconda-mode-conf ()
   (use-package anaconda-mode
-    :config
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-    
+    :ensure t
     )
+  )
+
+(defun company-anaconda-conf ()
+  (use-package company-anaconda
+    :ensure t))
+
+(defun enable-python-mode ()
+  ";;; Code: define packages needed."
+  (check-packages)
+  (anaconda-mode-conf)
+  (company-anaconda-conf)
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  
   )
 
 ;;; python.el ends here
