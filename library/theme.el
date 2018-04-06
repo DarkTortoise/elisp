016510aa627f24d573d0911c1e312227b324ddeb
algo4 <python@blogdaily.net>
2018-04-06 18:42:03 +0800

    add theme

--- library/theme.el
+++ library/theme.el
@@ -1,14 +1,8 @@
-(defun enable-base16 ()
-  (use-package base16-theme
-    :ensure t
-    :config
-    (load-theme 'base16-atelier-sulphurpool t)))
-
 (defun enable-theme (turn-on)
   (if turn-on
       (progn
 	(tool-bar-mode -1)
-	(enable-base16)
+	(load-theme 'base16-atelier-sulphurpool t)
 	(scroll-bar-mode -1))
     (progn
       (tool-bar-mode 1)
